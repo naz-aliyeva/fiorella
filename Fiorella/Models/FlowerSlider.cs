f553d16d5e16410a9efac0a7529c5e38711249fa
damirof <feriddmrv1@gmail.com>
2025-05-07 18:07:56 +0400

    front

---
+++ Fiorella/Models/FlowerSlider.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiorella.Models;
+
+public class FlowerSlider : BaseModel
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string ImgUrl { get; set; }
+
+}
